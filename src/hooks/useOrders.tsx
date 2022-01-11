dca66d7eb45db645062b645cf7e2f39f35e9dfa0
Chris Watson <chris.watson@chartiq.com>
2022-01-11 12:21:20 +0000

    useOrders changes to get fills processed. This This will be removed within the next couple of commits

--- src/hooks/useOrders.tsx
+++ src/hooks/useOrders.tsx
@@ -1,7 +1,8 @@
 import { addContextListener } from '@finos/fdc3';
+import { current, original } from 'immer';
 import { useCallback, useEffect } from 'react'
 import { useImmerReducer } from 'use-immer';
-import { Order, OrderContext } from '../types/orders';
+import { Allocation, Order, OrderContext } from '../types/orders';
 import { getRandomIntInclusive } from '../utils';
 
 interface Props {
@@ -18,17 +19,33 @@ export default function useOrders(props: Props) {
         case "add":
           draft.push(action.order)
           break;
+
         case "fill":
-          //Todo:test this
-          const orderFill = draft.find((order) => order.orderId === action.id);
-          orderFill?.fills.push(action.placement)
+          //From the order get a placement remove it from the placements array and move it to the fills array
+          const orderFillIndex = draft.findIndex((order) => order.orderId === action.orderId);
+
+          const newFill = original(draft)?.[orderFillIndex]?.allocations[0]
+
+          if (!newFill) return
+
+          draft[orderFillIndex].fills.push(newFill)
+          draft[orderFillIndex].placements.shift()
+          return draft
           break;
+
         case "placement":
-          //Todo:test this
-          const orderPlacement = draft.find((order) => order.orderId === action.id);
-          orderPlacement?.placements.push(action.placement)
+          //From the order get an allocation remove it from the allocation array and move it to the placements array
+
+          const placementIndex = draft.findIndex((order) => order.orderId === action.orderId);
+          draft[placementIndex].placements.push(action.allocation)
+
           break;
+        case "deleteAllocation":
+          const AllocationOrderIndex = draft.findIndex((order) => order.orderId === action.orderId);
+          draft[AllocationOrderIndex].allocations.shift()
+          break
         default:
+
           console.warn("no action taken - order reducer")
           break;
       }
@@ -36,13 +53,11 @@ export default function useOrders(props: Props) {
     defaultValue
   );
 
+
   const addOrder = useCallback((newOrder: Order) => {
     // check to make sure it's not already in the orders before we try and add it
-
-    const index = orders.findIndex((order) => newOrder.orderId === order.orderId);
-
+    const index = orders.find((order) => newOrder.orderId === order.orderId);
     if (index) return
-
 
     dispatch({
       type: "add",
@@ -51,22 +66,22 @@ export default function useOrders(props: Props) {
 
   }, [dispatch, orders]);
 
-  const addFill = useCallback(({ orderId, placement }: { orderId: string, placement: any }) => {
+  const addFill = useCallback((orderId: string | number) =>
     dispatch({
       type: "fill",
-      orderId,
-      placement
-    });
-  }, [dispatch]);
+      orderId
+    })
+    , [dispatch]);
 
-  const addPlacement = useCallback(({ orderId, allocation }: { orderId: string, allocation: any }) => {
+  const addPlacement = useCallback((orderId: string | number, allocation: Allocation) =>
     dispatch({
       type: "placement",
       orderId,
       allocation
-    });
-  }, [dispatch]);
+    })
+    , [dispatch]);
 
+  // automatically subscribe to any context being sent - for linker selecting (user led)
   useEffect(() => {
     const listener = addContextListener("finsemble.order", (context: OrderContext) => {
 
@@ -74,11 +89,12 @@ export default function useOrders(props: Props) {
       console.log(context?.order?.appName, appName)
 
       if (!context.order) return
+
+      // only add orders if they come from a different app or if they come from the Combined blotter
       if (context.order.appName !== "Combined" && context.order.appName === appName) return;
 
-      const { order } = context
-      console.log(order)
-      addOrder(order)
+      console.log(context.order)
+      addOrder(context.order)
     })
     return () => {
       listener.unsubscribe()
@@ -93,13 +109,21 @@ export default function useOrders(props: Props) {
    * To place/fill choose a random time value and then start pushing into the placement or fill array.
    *
    */
-  const startPlacementAndFill = (orderId: string) => {
-    const index = orders.findIndex((order) => orderId === order.orderId);
-    const order = orders[index]
+  const startPlacementAndFill = useCallback((orderId: string | number) => {
+    const order = orders.find((order) => orderId === order.orderId);
+
+    if (!order) return
 
     if (order.allocations.length !== 0) {
+      console.log(order.allocations)
       setTimeout(() => {
         //fill order
+        const allocation = order.allocations[0]
+        addPlacement(orderId, allocation)
+        dispatch({
+          type: "deleteAllocation",
+          orderId
+        })
         startPlacementAndFill(orderId)
       }, getRandomIntInclusive(500, 3000));
     };
@@ -107,15 +131,20 @@ export default function useOrders(props: Props) {
     if (order.placements.length !== 0) {
       setTimeout(() => {
         //fill order
+        addFill(orderId)
         startPlacementAndFill(orderId)
       }, getRandomIntInclusive(500, 3000));
     }
-  }
+  }, [addFill, addPlacement, dispatch, orders])
 
 
   //use effect once to map through all orders and kick off the placementAndFill
 
-
+  useEffect(() => {
+    orders.forEach(({ orderId }) => {
+      // startPlacementAndFill(orderId)
+    })
+  }, [])
 
 
   /**
@@ -143,7 +172,8 @@ export function percentComplete(order: Order) {
 
   const total = allocations.length + placements.length + fills.length || 0
 
+
   if (total === 0) return total
 
-  return Math.round((total / fills.length) * 100)
+  return Math.round((fills.length / total) * 100)
 }
