cc19fbc1cf86724a57185627a9fe5bdcb9a82112
Chris Watson <chris.watson@chartiq.com>
2022-01-11 12:21:52 +0000

    update order types to match updated labels.
    data changes also included

--- src/types/orders.d.ts
+++ src/types/orders.d.ts
@@ -5,32 +5,34 @@ export interface OrderContext extends Context {
 
 
 export interface Order {
-  "orderId": string | number,
-  "appName": string,
-  "securityName": string,
-  "securityId": string | number,
-  "targetPrice": string | number,
-  "targetQuantity": string | number,
-  "targetAmount": string | number,
-  "manager": string,
-  "trader": string,
-  "tradeDate": string | Date,
-  "account": string,
-  "status": "NEW", "WORKING", "FILLED",
-  "broker": string,
-  "securityType": string,
-  "transactionType": "BUY" | "SELL",
-  "createDate": string | Date,
-  "allocations": Array<{ Allocation }>,
-  "placements": Array<{ Allocation }>,
-  "fills": Array<{ Allocation }>
+  appName: string,
+  orderId: string | number,
+  ticker: string;
+  securityId: string | number,
+  targetPrice: string | number,
+  targetQuantity: string | number,
+  targetAmount: string | number,
+  manager: string,
+  trader: string,
+  tradeDate: string | Date,
+  settlementDate: string | Date;
+  account: string,
+  status: "NEW", "WORKING", "FILLED",
+  executedQuantity: number | string;
+  broker: string,
+  securityType: string,
+  transactionType: "BUY" | "SELL",
+  createDate: string | Date,
+  allocations: Array<Allocation>,
+  placements: Array<Allocation>,
+  fills: Array<Allocation>
 }
 
 
 interface Allocation {
-  "tradeId": string | number,
-  "account": string,
-  "targetQuantity": string | number,
-  "targetPrice": string | number,
-  "targetAmount": string | number
+  tradeId: string | number,
+  account: string,
+  targetQuantity: string | number,
+  targetPrice: string | number,
+  targetAmount: string | number
 }
